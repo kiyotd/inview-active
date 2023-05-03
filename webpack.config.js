b3cef9031e5ef42b7e2d981164c7c0d13497955c
kiyotd <torigoedesign@gmail.com>
2023-05-03 14:29:13 +0900

    feat(package.json, webpack.config.js): add package.json and webpack.config.js files for the project
    Add package.json file with project metadata, scripts, dependencies, and devDependencies. Add webpack.config.js file with webpack configuration for the project, including entry point, output, and plugins.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+const mode = process.argv[process.argv.indexOf("--mode") + 1];
+
+module.exports = {
+  mode: mode,
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.ts', 'js']
+  },
+  module: {
+    rules: [{test: /\.ts$/, loader: 'ts-loader'}]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'kiyotd/scroll-toggle-active-class',
+    libraryTarget: 'umd',
+    globalObject: 'typeof self !== "undefined" ? self : this'
+  },
+  plugins: [new BrowserSyncPlugin({
+    host: "localhost",
+    port: 3010,
+    files: [
+      `dist/**/*.html`,
+      `dist/**/*.jpg`,
+      `dist/**/*.png`,
+      `dist/**/*.js`,
+      `dist/**/*.css`,
+    ],
+  }),
+  ]
+};
